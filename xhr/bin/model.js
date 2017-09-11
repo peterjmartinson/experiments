4d3598d2f5065fd6e106327cc56f18f4766fa0f5
Peter Martinson <peter.j.martinson@gmail.com>
2017-09-11 08:24:25 -0400

    Give all model methods callbacks.
    You need this because they are asynchronous.

--- xhr/bin/model.js
+++ xhr/bin/model.js
@@ -7,64 +7,77 @@
       dbfunction = {};
 
   // todo: autopopulate the ID
-  dbfunction.createItem = function (new_item) {
+  dbfunction.createItem = function (new_item, callback) {
+    let response = { success: false, message: '', item: new_item };
     if (db.push(new_item)) {
-      return 'New item ' + JSON.stringify(new_item) + ' has been added';
+      response.success = true;
+      response.message = 'New item ' + JSON.stringify(new_item) + ' has been added';
+      callback(response);
     }
     else {
-      return 'Error: Item not added!';
+      response.message = 'Error: Item not added!';
+      callback(response);
     }
   };
 
-  dbfunction.updateItem = function (item_id, updated_item) {
+  dbfunction.updateItem = function (item_id, updated_item, callback) {
     let item_index = findIndex(item_id);
     let response = { success: false, message: '', item: updated_item };
     if (item_index < 0) {
       response.message = 'Item not found';
-      return response;
+      callback(response);
     }
     else if ( !updated_item || typeof updated_item !== 'string') {
       response.message = 'Error - incorrect format';
-      return response;
+      callback(response);
     }
     else {
       db[item_index] = { id: item_id, item: updated_item };
       response.success = true;
       response.message = 'Success!';
-      return response;
+      callback(response);
     }
   };
 
-  dbfunction.readAllItems = function () {
-    return db;
+  dbfunction.readAllItems = function (callback) {
+    callback(db);
   };
 
-  dbfunction.readItem = function (item_id) {
+  dbfunction.readItem = function (item_id, callback) {
     let item_index = findIndex(item_id);
-    if (item_index < 0) {
-      return "Item not found";
-    }
-    else {
-      return db[item_index];
-    }
-  };
-
-  dbfunction.deleteItem = function (item_id) {
-    let item_index = findIndex(item_id);
-    let response = { success: false, message: '', item: db[item_id] || {} };
+    let response = { success: false, message: '', item: db[item_index] || {} };
     if (item_index < 0) {
       response.message = 'Item not found';
-      return response;
+      callback(response);
+    }
+    else {
+      response.success = true;
+      response.message = 'Item successfully found';
+      response.item = db[item_index];
+      callback(response);
+    }
+  };
+
+  dbfunction.deleteItem = function (item_id, callback) {
+    let item_index = findIndex(item_id);
+    let response = { success: false, message: '', item: {} };
+    if (item_index < 0) {
+      response.message = 'Item not found';
+      callback(response);
     }
     else {
       db.splice(item_index, 1);
+      response.item = db[item_index];
       response.success = true;
       response.message = 'Success!';
-      return response;
+      callback(response);
     }
   };
 
   function findIndex(item_id) {
+    if ( item_id < 1) {
+      return -1;
+    }
     for (let i = 0; i < db.length; i++) {
       if (db[i].id === item_id) return i;
     }
--- xhr/test/model.test.js
+++ xhr/test/model.test.js
@@ -33,9 +33,12 @@ describe('model.js', function() {
 });
 
 describe('model.js - readAllItems()', function() {
-  it('should return the whole database', function() {
+  it('should return the whole database', function(done) {
     let dump = [];
-    dump = model.readAllItems();
+    dump = model.readAllItems(function(data) {
+      done();
+      return data;
+    });
     console.log("length: " + dump.length);
     console.log("dump[1]: " + JSON.stringify(dump[1]));
     assert.equal(dump.length, 3);
@@ -44,52 +47,81 @@ describe('model.js - readAllItems()', function() {
 });
 
 describe('model.js - readItem()', function() {
-  it('should return a specific item', function() {
-    let item = model.readItem(2);
-    assert.equal(item.item, second_item.item);
+  it('should return a specific item', function(done) {
+    let response = model.readItem(2, function(data) {
+      done();
+      return data;
+    });
+    assert.equal(response.item.item, second_item.item);
+    assert.ok(response.success);
   });
 
-  it('should respond gracefully to bad requests', function() {
-    let item = model.readItem(-7);
-    assert.equal(item, 'Item not found');
+  it('should respond gracefully to bad requests', function(done) {
+    let response = model.readItem(-7, function(data) {
+      done();
+      return data;
+    });
+    assert.ok(!response.success);
   });
 });
 
 describe('model.js - createItem()', function() {
-  it('should create a new item', function() {
+  it('should create a new item', function(done) {
     let new_item = {id:4, item: "Fourth test item"};
-    model.createItem(new_item);
+    response = model.createItem(new_item, function(data) {
+      done();
+      return data;
+    });
     assert.equal(db.length, 4);
+    assert.ok(response.success);
   });
 });
 
 describe('model.js - updateItem()', function() {
-  it('should update an existing item', function() {
+  it('should update an existing item', function(done) {
     let old_item = db[0],
         updated_item = "Test",
-        response = model.updateItem(1, updated_item),
+        response = model.updateItem(1, updated_item, function(data) {
+          done();
+          return data;
+        }),
         new_item = db[0];
     assert.notDeepEqual(old_item, new_item);
     assert.ok(response.success);
   });
-  it('should respond gracefully to bad requests', function() {
+  it('should respond gracefully to bad id', function(done) {
     let bad_id = 100,
         good_id = 1,
         bad_item = {},
         good_item = {id:1, item: "Another Test"},
-        response_bad_id = model.updateItem(bad_id, good_item),
-        response_bad_item = model.updateItem(good_id, bad_item);
+        response_bad_id = model.updateItem(bad_id, good_item, function(data) {
+          done();
+          return data;
+        });
     assert.equal(response_bad_id.success, false);
+  });
+  it('should respond gracefully to bad requests', function(done) {
+    let bad_id = 100,
+        good_id = 1,
+        bad_item = {},
+        good_item = {id:1, item: "Another Test"},
+        response_bad_item = model.updateItem(good_id, bad_item, function(data) {
+          done();
+          return data;
+        });
     assert.equal(response_bad_item.success, false);
   });
 });
 
 describe('model.js - deleteItem()', function() {
-  it('should remove an item from the list', function() {
+  it('should remove an item from the list', function(done) {
     let deleted_item = db[0];
-    let response = model.deleteItem(1);
+    let response = model.deleteItem(1, function(data) {
+      done();
+      return data;
+    });
     assert.notDeepEqual(db[0], deleted_item);
-    assert.equal(response.success, true);
+    assert.ok(response.success);
   });
 });
 
