c94be07f3e23e1e291a18ffb56edf5006f7358d6
peter martinson <peter martinson>
2017-09-11 21:17:45 -0400

    Add another test file

--- xhr/data/metadata.js
+++ xhr/data/metadata.js
@@ -3,8 +3,9 @@
   'use strict';
 
   let database = [
-    { id: 1, item: 'First Todo', file_name:'test.md' },
-    { id: 2, item: 'Second Todo', file_name:'test2.md' }
+    { id: 1, item: 'First Article', file_name:'test.md' },
+    { id: 2, item: 'Second Article', file_name:'test2.md' },
+    { id: 3, item: 'Second Article', file_name:'test3.md' }
   ];
 
   module.exports = database || [];
